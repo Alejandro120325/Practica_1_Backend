90377408b00d9cc5bc356544e6034cba4a793d08
Alejandro Ojeda <alejandro.ojedaherrera@gmail.com>
2026-07-07 11:49:51 -0500

    Frontend conectado a backend AWS

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,12 @@
 ﻿import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_URL =
+    import.meta.env.MODE === "aws"
+        ? "http://18.191.247.48:3000"
+        : import.meta.env.VITE_API_URL || "http://localhost:3000";
+
+console.log("MODO VITE:", import.meta.env.MODE);
+console.log("API_URL usada por el frontend:", API_URL);
 
 export const api = axios.create({
   baseURL: API_URL,
@@ -14,4 +20,3 @@ export const endpoints = {
   gastos: "/misitio/gastos",
   usuarios: "/misitio/usuarios",
 };
-
